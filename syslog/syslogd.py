77053a853d5bea26b505be696fc2a09f9b889ad1
Mathieu Lecarme <mlecarme@bearstech.com>
2017-04-20 10:22:34 +0200

    Broken usage of fake /dev/log

--- syslog/syslogd.py
+++ syslog/syslogd.py
@@ -2,20 +2,34 @@
 
 # See https://gist.github.com/marcelom/4218010
 import socketserver
+import os
 
 HOST, PORT = "0.0.0.0", 514
 
 
-class SyslogUDPHandler(socketserver.BaseRequestHandler):
+class SyslogHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         data = bytes.decode(self.request[0].strip())
-        socket = self.request[1]
-        print( "%s : %s " % (self.client_address[0], str(data)))
+        #socket = self.request[1]
+        if self.client_address is not None:
+            print("%s : %s " % (self.client_address[0], str(data)))
+        else:
+            print(str(data))
+
 
 if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == "--unix":
+        try:
+            os.remove('/tmp/log')
+        except:
+            pass # At least, we trised
+        server = socketserver.UnixDatagramServer('/tmp/log', SyslogHandler)
+        print('listen socket')
+    else:
+        server = socketserver.UDPServer((HOST,PORT), SyslogHandler)
     try:
-        server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
         server.serve_forever(poll_interval=0.5)
     except (IOError, SystemExit):
         raise
